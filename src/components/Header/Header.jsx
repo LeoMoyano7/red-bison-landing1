fcd7ffadb4bffd1876ff820954818b32e8d17528
LeoMoyano7 <leonardo.moyano@thefuzzyfish.com>
2021-04-09 18:02:15 -0300

    update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <header className="row header-color">
+                <ul className="row decoration-none">
+                    <li>dfksdifsdf</li>
+                    <li>dfksdifsdf</li>
+                    <li>dfksdifsdf</li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Header
