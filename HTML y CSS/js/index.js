cf1ebbcc5d8d8cc7c7d2d1588cc644244ead5a3c
Nicolas Gonzalez <nicolas.gonzalez@thefuzzyfish.com>
2021-04-30 15:23:24 -0300

    Fix delay icons effects and duration of scroll efect

--- HTML y CSS/js/index.js
+++ HTML y CSS/js/index.js
@@ -1,30 +1,59 @@
 window.sr = ScrollReveal();
   sr.reveal('.scroll-bottom', {
-    duration: 3000,
+    duration: 1400,
     origin: 'bottom',
     distance: '-100px'
   });
 
   sr.reveal('.scroll-right', {
-    duration: 3000,
+    duration: 1400,
     origin: 'right',
     distance: '-100px'
   });
   sr.reveal('.scroll-left', {
-    duration: 3000,
+    duration: 1400,
     origin: 'left',
     distance: '-100px'
   });
   sr.reveal('.scroll-top', {
-    duration: 3000,
+    duration: 1400,
     origin: 'top',
     distance: '-100px'
   });
+  sr.reveal('.scroll-top-icon-0', {
+    duration: 1400,
+    origin: 'top',
+    distance: '-100px'
+  });
+  sr.reveal('.scroll-top-icon-1', {
+    duration: 1400,
+    origin: 'top',
+    distance: '-100px',
+    delay: 300
+  });
+  sr.reveal('.scroll-top-icon-2', {
+    duration: 1400,
+    origin: 'top',
+    distance: '-100px',
+    delay: 600
+  });
+  sr.reveal('.scroll-top-icon-3', {
+    duration: 1400,
+    origin: 'top',
+    distance: '-100px',
+    delay: 900
+  });
+  sr.reveal('.scroll-top-icon-4', {
+    duration: 1400,
+    origin: 'top',
+    distance: '-100px',
+    delay: 1200
+  });
   sr.reveal('.scroll-top-50', {
-    duration: 3000,
+    duration: 1400,
     origin: 'top',
     distance: '-50px'
   });
   sr.reveal('.scroll-fix', {
-    duration: 3000,
+    duration: 1400,
   });
